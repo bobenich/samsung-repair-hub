221a147887484fc84e5a6027051cb5475511e26f
bobenich <151072496+bobenich@users.noreply.github.com>
2025-03-20 21:13:00 +0300

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -27,7 +27,7 @@ const Header = () => {
     { label: 'Главная', href: '#hero' },
     { label: 'Услуги', href: '#services' },
     { label: 'О нас', href: '#about' },
-    { label: 'Опрос', href: '#survey' },
+    { label: 'Рассчёт стоимости', href: '#survey' },
     { label: 'Контакты', href: '#contacts' }
   ];
 
